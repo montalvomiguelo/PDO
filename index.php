7ccd98c83822255712b0d511771a21ab6bd27f25
imontalvomiguel <ubaldo.im@gmail.com>
2015-04-17 16:43:55 -0500

    Paginación, jugando PHP Data Objects

--- index.php
+++ index.php
@@ -3,13 +3,53 @@
 require_once('database.php');
 
 /**
+ * Variables de la paginación
+ */
+
+// Límite de resultados de búsqueda.
+$limit = 10;
+
+// Número de página de $_GET
+if ( isset( $_GET['p'] ) ) {
+  $p = $_GET['p'];
+} else {
+  $p = 1;
+}
+
+// Variable para el query
+$start = ($p-1) * $limit;
+
+/**
+ * Get Data
+ */
+
+/**
   * Es mejor práctica usar el constructor try / catch cada vez
   * que interactuamos con nuestra base de datos.
  */
 try {
   // PDO tiene un método para hacer consultas
   // Retorna un objeto de tipo PDOStatement
-  $results = $pdo->query('SELECT * FROM film');
+  $results = $pdo->prepare('SELECT * FROM film ORDER BY film_id LIMIT :start, :limit');
+
+  // Vincula el nombre de un valor en la consulta con su variable.
+  $results->bindValue(':start', $start, PDO::PARAM_INT);
+  $results->bindValue(':limit', $limit, PDO::PARAM_INT);
+
+  // Ejecutar la consulta
+  $results->execute();
+
+} catch (Exception $e) {
+
+  echo $e->getMessage();
+  die();
+
+}
+
+// Obtener la cantidad de filas
+try {
+  $rows_query = $pdo->prepare('SELECT COUNT(film_id) FROM film');
+  $rows_query->execute();
 } catch (Exception $e) {
   echo $e->getMessage();
   die();
@@ -21,7 +61,18 @@ try {
  * PDO:FETCH_NUM hace que solamente tiene enteros para las llaves
  */
 $films = $results->fetchAll(PDO::FETCH_ASSOC);
+$rowsCount = $rows_query->fetchColumn();
+//var_dump($rows);
 //var_dump($films);
+
+/**
+ * Listado de páginas
+ */
+$pagesTotal = (ceil( $rowsCount / $limit ));
+
+$pagesLimit = ceil($p / $limit) * $limit;
+$pagesStart = $pagesLimit - $limit + 1;
+
 ?>
 
 <!DOCTYPE html>
@@ -49,6 +100,34 @@ $films = $results->fetchAll(PDO::FETCH_ASSOC);
       <?php endforeach; ?>
       </tbody>
     </table>
+    <nav>
+      <ul class="pagination">
+
+        <li class="<?php echo ($pagesLimit <= $limit ? 'disabled' : ''); ?>">
+          <a href="<?php echo ( $pagesStart > $limit  ? '?p=' . ($pagesStart - 1) : '' ); ?>" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+
+        <?php for ($i = $pagesStart; $i <= $pagesLimit; $i++) : ?>
+        <li class="<?php echo ($p == $i ? 'active' : ''); ?>"><a href="?p=<?php echo $i; ?>"><?php echo $i; ?> <span class="sr-only">(current)</span></a></li>
+        <?php endfor; ?>
+
+        <li class="<?php echo ($pagesLimit >= $pagesTotal ? 'disabled' : ''); ?>">
+          <a href="<?php echo ($pagesLimit < $pagesTotal ? '?p=' . ($pagesLimit + 1): ''); ?>" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+
+      </ul>
+    </nav>
   </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <script>
+    $('.pagination .disabled a, .pagination .active a').on('click', function(e) {
+      e.preventDefault();
+    });
+  </script>
 </body>
 </html>
